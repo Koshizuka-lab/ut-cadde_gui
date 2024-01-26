6f218b638bbc74813e99445b27eba78c9e1f4d2a
kamano905 <katsu0422.amano@gmail.com>
2024-01-26 10:19:14 +0900

    feat: redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import { NextPage } from "next";
 import Cookies from "js-cookie";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const Page: NextPage = () => {
-  Cookies.remove("access_token");
-  Cookies.remove("refresh_token");
-  Cookies.remove("user_id");
+  const router = useRouter();
+  useEffect(() => {
+    Cookies.remove("access_token");
+    Cookies.remove("refresh_token");
+    Cookies.remove("user_id");
+    router.push("/signin");
+  }, []);
+
   return <div>Index Page
     {Cookies.get("access_token")}
     {Cookies.get("refresh_token")}
